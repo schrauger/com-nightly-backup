a21f51d162b3fc2651b3df7cf38fc6c428ebf62b
Stephen Schrauger <github@schrauger.com>
2015-08-19 11:40:47 -0400

    adds configurable backup length

--- sample.config.py
+++ sample.config.py
@@ -4,11 +4,13 @@ sites['site_med_prd'] = {
 	'directory': "med.ucf.edu/production", 
 	'user': "DB_USER", 
 	'password': "DB_PASSWORD",
-	'db': "DB"
+	'db': "DB",
+	'backup_days': "90"
 }
 sites['site_med_dev'] = {
 	'directory': "med.ucf.edu/dev", 
 	'user': "DB_DEV_USER",
 	'password': "DB_DEV_PASSWORD",
-	'db': "DB_DEV"
+	'db': "DB_DEV",
+	'backup_days': "5"
 }
