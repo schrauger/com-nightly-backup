a4e5411640488821bf18818b58aa5e369a579d21
Stephen Schrauger <stephen@schrauger.com>
2021-01-11 11:55:02 -0500

    fixes trailing slash

--- nightly_backup.py
+++ nightly_backup.py
@@ -64,8 +64,8 @@ def run_backup(site):
 		# get the relative path of the file based on the string
 		protected_path = os.path.dirname(protected_file)
 		protected_filename = os.path.basename(protected_file)
-		protected_path_full_origin = nightly_dir + '/web/' + protected_path
-		protected_path_full_destination = nightly_dir + '/protected/' + protected_path
+		protected_path_full_origin = nightly_dir + '/web/' + protected_path + '/'
+		protected_path_full_destination = nightly_dir + '/protected/' + protected_path + '/'
 		#print(protected_path_full_origin + protected_file)
 		#print(protected_path_full_destination + protected_file)
 		os.system('sudo -u ' + site['linux_user'] + ' ' + 'mkdir -p "' + protected_path_full_destination + '"')
